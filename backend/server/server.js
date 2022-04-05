75452fc7c03ddf1f941c5758d489b4bd58b806b5
Jonathan <alkalayjonathan28@yahoo.com>
2022-04-06 02:33:29 +0300

    added type for business support

--- backend/server/server.js
+++ backend/server/server.js
@@ -6,6 +6,7 @@ const Userdb = require('../schemas/user_schema');
 app.use(cors());
 
 const mongoose = require('mongoose');
+const e = require('cors');
 const mongoDB = "mongodb+srv://jonathan:Aa123654!@cluster0.2lj7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -82,13 +83,16 @@ app.get('/logIn=:userName&:passWord&:type', (request, response) => {
                 "message": "incorrect type provided"
             }); 
         }
+       
 });
 
 
 app.get('/logOut=:userName&:type', (request, response) => {
 
     const userName = request.params.userName;
+    const type = request.params.type;
 
+    if(type === 'user'){
     Userdb.findOne({ userName: userName })
         .then((usr) => {
 
@@ -99,14 +103,38 @@ app.get('/logOut=:userName&:type', (request, response) => {
                     "status": true,
                     "message": "logged out successfully"
                 });
-                return;
+            }else{
+                response.send({
+                    "status": false,
+                    "message": "user name is invalid"
+                })
+            }
+        });
+    }else if(type === "business"){
+        Businessdb.findOne({ userName: userName })
+        .then((bsns) => {
+
+            if(bsns != null){
+                bsns.connected = false;
+                bsns.save();
+                response.send({
+                    "status": true,
+                    "message": "logged out successfully"
+                });
+            }else{
+                response.send({
+                    "status": false,
+                    "message": "user name is invalid"
+                })
             }
         });
 
-    response.send({
-        "status": false,
-        "message": "incorrect username provided"
-    });
+    }else{
+        response.send({
+            "status": false,
+            "message": "incorrect username provided"
+        });
+    }
 });
 
 
@@ -114,7 +142,9 @@ app.get('/createAccount=:userName&:passWord&:type', (request, response) => {
 
     const username = request.params.userName;
     const password = request.params.passWord;
+    const type = request.params.type;
 
+    if (type === 'user'){
     Userdb.findOne({userName: username})
         .then((usr) => {
 
@@ -123,14 +153,26 @@ app.get('/createAccount=:userName&:passWord&:type', (request, response) => {
                     "status": false,
                     "message": "User already exists"
                 })
-                return;
-            }
-
-            Userdb.create({ userName: username, passWord: password, connected: false})    
+            }else
+                Userdb.create({ userName: username, passWord: password, connected: false});    
         });
+    }else if(type === 'business'){
 
-    response.send({
-        "status": true, 
-        "message": "user created successfully"
-    });
+        Businessdb.findOne({userName: username})
+        .then((bsns) => {
+
+            if(bsns != null){
+                response.send({
+                    "status": false,
+                    "message": "Business already exists"
+                })
+            }else
+                Businessdb.create({ userName: username, passWord: password, connected: false});    
+        });
+    }else{
+        response.send({
+            "status": true, 
+            "message": "account created successfully"
+        });
+    }
 });
